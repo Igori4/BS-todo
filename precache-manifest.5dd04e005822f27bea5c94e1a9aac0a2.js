188499e9494d7e864f59d6085a02fb4381f26228
Igori4 <igorpilyak@gmail.com>
2019-07-11 00:06:30 +0300

    Updates

---
+++ precache-manifest.5dd04e005822f27bea5c94e1a9aac0a2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffb542a0690bf55c900a2bdc7ddb4d21",
+    "url": "/BS-todo/index.html"
+  },
+  {
+    "revision": "da09c3271962c4d29d6b",
+    "url": "/BS-todo/static/css/main.dd02db69.chunk.css"
+  },
+  {
+    "revision": "123fb41c3fb46a6d1e24",
+    "url": "/BS-todo/static/js/2.a5adb38c.chunk.js"
+  },
+  {
+    "revision": "da09c3271962c4d29d6b",
+    "url": "/BS-todo/static/js/main.10141631.chunk.js"
+  },
+  {
+    "revision": "7f5e906a8fc6d01de1dd",
+    "url": "/BS-todo/static/js/runtime~main.a207630c.js"
+  }
+]);
