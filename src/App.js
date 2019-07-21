8bf140d51c531b544da70556d31b026caee32c86
Igori4 <igorpilyak@gmail.com>
2019-07-21 23:23:07 +0300

    add styles for mobile screen

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import React from 'react';
 import './App.css';
 import Greeting from './components/Greeting';
 import Todo from './components/Todo';
+import Footer from './components/Footer';
 
 
 function App() {
   return (
+    <>
     <main>
       <Greeting />
       <Todo />
     </main>
+    <Footer />
+    </>
   )
 };
 
---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Footer(props) {
+  return (
+    <footer className="footer">
+      <div className="footer-block">
+        <p>
+          Todo-App 2019.
+        </p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer;
